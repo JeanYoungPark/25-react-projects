7bc85718776bb88cbe6bb9772cc24c2d20313c62
jean young Park <jean.young.park.96@gmail.com>
2024-02-14 15:18:11 +0900

    feat: use fetch hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import FeatureFlags from './components/feature-flag';
 import FeatureFlagGlobalState from './components/feature-flag/context';
+import UseFetchHookTest from './components/use-fetch/fetch-test';
 
 function App() {
 	return (
 		<div className="App">
 			{/* Feature Flag Implementation */}
-			<FeatureFlagGlobalState>
+			{/* <FeatureFlagGlobalState>
 				<FeatureFlags />
-			</FeatureFlagGlobalState>
+			</FeatureFlagGlobalState> */}
+			{/* UseFetch custom hook */}
+			<UseFetchHookTest />
 		</div>
 	);
 }
---
+++ src/components/use-fetch/fetch-test.jsx
@@ -0,0 +1,19 @@
+import useFetch from "."
+
+export default function UseFetchHookTest() {
+    const {data, error, pending} = useFetch('https://dummyjson.com/products', {})
+
+    return (
+        <div>
+            <h1>Use Fetch Hook</h1>
+            {
+                pending ? <h3>Pending ! Please wait</h3>: null
+            }
+            {
+                data && data.products && data.products.length
+                ? data.products.map((productItem, index) => <p key={index}>{productItem.title}</p>)
+                : null
+            }
+        </div>
+    )
+}
